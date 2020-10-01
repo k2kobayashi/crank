2bf871b6d70c61b9c953394dd53f5003f57c8934
Kazuhiro KOBAYASHI <root.4mac@gmail.com>
2020-10-01 14:18:12 +0900

    update

--- setup.py
+++ setup.py
@@ -3,20 +3,21 @@
 from setuptools import setup, find_packages
 
 import os
-if os.path.exists('../README.md'):
-    README = open('../README.md').read()
+
+if os.path.exists("../README.md"):
+    README = open("../README.md").read()
 else:
     README = ""  # a placeholder, readme is generated on release
-CHANGES = open('CHANGES.md').read()
+CHANGES = open("CHANGES.md").read()
 
 setup(
     name="crank",
-    version="0.20",
+    version="0.3",
     description="Non-parallel Voice Conversion",
-    url='https://github.com/k2kobayashi/crank',
-    author='K. KOBAYASHI',
-    packages=find_packages(exclude=['shell, stage']),
-    long_description=(README + '\n' + CHANGES),
-    license='MIT',
-    install_requires=open('tools/requirements.txt').readlines(),
+    url="https://github.com/k2kobayashi/crank",
+    author="K. KOBAYASHI",
+    packages=find_packages(exclude=["shell, stage"]),
+    long_description=(README + "\n" + CHANGES),
+    license="MIT",
+    install_requires=open("tools/requirements.txt").readlines(),
 )
