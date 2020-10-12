cb981f3502c06339d64e182e480efe3bf23e56e6
Kazuhiro KOBAYASHI <root.4mac@gmail.com>
2020-10-12 10:06:41 +0900

    fix test

--- test/test_feature.py
+++ test/test_feature.py
@@ -13,7 +13,6 @@
 
 from pathlib import Path
 
-
 from crank.feature import Feature
 from crank.utils import load_yaml
 
@@ -23,7 +22,9 @@ datadir = Path(__file__).parent / "data"
 def test_feature():
     conf = load_yaml(datadir / "mlfb_vqvae.yml")
     spkr_conf = load_yaml(datadir / "spkr.yml")
-    feat = Feature(datadir, conf["feature"], spkr_conf["SF1"], gl_flag=True)
+    feat = Feature(
+        datadir, conf["feature"], spkr_conf["SF1"], gl_flag=True, synth_flag=True
+    )
     feat.analyze(datadir / "SF1_10001.wav")
     (datadir / "SF1_10001.h5").unlink()
     (datadir / "SF1_10001_anasyn.wav").unlink()
