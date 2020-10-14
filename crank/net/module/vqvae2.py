1e0ce9ea11dc763bf029fa17cb66bdbe513cf43e
Kazuhiro KOBAYASHI <root.4mac@gmail.com>
2020-10-14 16:21:10 +0900

    fix

--- crank/net/module/vqvae2.py
+++ crank/net/module/vqvae2.py
@@ -40,7 +40,10 @@ class VQVAE2(nn.Module):
         x = x.transpose(1, 2)
         if spkrvec is not None:
             spkremb = self.spkr_embedding(spkrvec)
-            dec_h = torch.cat([dec_h, spkremb], axis=-1)
+            if dec_h is not None:
+                dec_h = torch.cat([dec_h, spkremb], axis=-1)
+            else:
+                dec_h = spkremb
         enc_h = enc_h.transpose(1, 2) if enc_h is not None else None
         dec_h = dec_h.transpose(1, 2) if dec_h is not None else None
 
@@ -57,10 +60,16 @@ class VQVAE2(nn.Module):
         x = x.transpose(1, 2)
         if org_spkrvec is not None:
             org_spkremb = self.spkr_embedding(org_spkrvec)
-            org_dec_h = torch.cat([org_dec_h, org_spkremb], axis=-1)
+            if org_dec_h is not None:
+                org_dec_h = torch.cat([org_dec_h, org_spkremb], axis=-1)
+            else:
+                org_dec_h = org_spkremb
         if cv_spkrvec is not None:
             cv_spkremb = self.spkr_embedding(cv_spkrvec)
-            cv_dec_h = torch.cat([cv_dec_h, cv_spkremb], axis=-1)
+            if cv_dec_h is not None:
+                cv_dec_h = torch.cat([cv_dec_h, cv_spkremb], axis=-1)
+            else:
+                cv_dec_h = cv_spkremb
         org_enc_h = org_enc_h.transpose(1, 2) if org_enc_h is not None else None
         org_dec_h = org_dec_h.transpose(1, 2) if org_dec_h is not None else None
         cv_enc_h = cv_enc_h.transpose(1, 2) if cv_enc_h is not None else None
